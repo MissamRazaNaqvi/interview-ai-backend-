8531656818a0fd9d585ca014e9c2cc383875a29d
misamraja.nakvi@krishtechnolabs.com <misamraja.nakvi@krishtechnolabs.com>
2026-06-22 16:10:43 +0530

    minor change in index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ import dbConnect from "./src/dbConfig/dbConnect.js";
 // import { app } from "./src/app.js";
 
 dbConnect()
-  // .then(() => {
-  //   app.listen(8000, () => {
-  //     console.log("Server running on port 8000");
-  //   });
-  // })
-  // .catch((error) => {
-  //   console.error("MongoDB connection failed:", error);
-  // });
+  .then(() => {
+    app.listen(8000, () => {
+      console.log("Server running on port 8000");
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection failed:", error);
+  });
 
